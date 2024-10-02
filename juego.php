0d3896a22047d2b5ef66f2b74cfbb5c1add2c19b
criss300 <crisstrabajos004@gmail.com>
2024-10-02 17:20:59 -0500

    2/10/2024 Se realizo el apartado de juego.php y se le agrego un poco de contenido  enter the commit message for your changes. Lines starting

--- juego.php
+++ juego.php
@@ -2,8 +2,20 @@
 <html lang="en">
 <head>
   <?php include('principal/head.php') ?>
+  <style>
+    .col-2{border: 1px solid #000} </style>
 </head>
 <body>
-    
+<div class="container">
+<div class="row col-12">
+    <div class="col-2 col-md-2">Columna 1</div>
+    <div class="col-2 col-md-2">Columna 2</div>
+    <div class="col-2 col-md-2">Columna 3</div>
+    <div class="col-2 col-md-2">Columna 4</div>
+    <div class="col-2 col-md-2">Columna 5</div>
+    <div class="col-2 col-md-2">Columna 6</div>
+  </div>
+</div>
+
 </body>
 </html>
