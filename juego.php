dc7e15f39dbf05c2b01ea592c2c75531095ead08
criss300 <cristianelpro004@gmail.com>
2024-10-03 20:01:12 -0500

    3/10/2024 se agrego imagen junto con una parte de la grid o intento de responsive junto a estilosJuego.css y su respectivo enlace a head.php

--- juego.php
+++ juego.php
@@ -2,19 +2,23 @@
 <html lang="en">
 <head>
   <?php include('principal/head.php') ?>
-  <style>
-    .col-2{border: 1px solid #000} </style>
+  <style>.col-2{border: 1px solid #000}    </style>
+  <style>.col-11{border: 1px solid #000} </style>
+    
 </head>
 <body>
-<div class="container">
-<div class="row col-12">
-    <div class="col-2 col-md-2">Columna 1</div>
-    <div class="col-2 col-md-2">Columna 2</div>
-    <div class="col-2 col-md-2">Columna 3</div>
-    <div class="col-2 col-md-2">Columna 4</div>
-    <div class="col-2 col-md-2">Columna 5</div>
-    <div class="col-2 col-md-2">Columna 6</div>
+ <div class="container">
+    <div class="row col-12">
+      <div class="col-2 col-md-2">Columna 2</div>
+       <div class="col-2 col-md-2">Columna 3</div>
+        <div class="col-2 col-md-4"> 
+         <div class="d-flex justify-content-center">
+         <img src="img/Pergamino-juego-head.png" alt="El pergamino head"/>
+        </div>
+        
+    </div>
   </div>
+   
 </div>
 
 </body>
--- principal/head.php
+++ principal/head.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>The god of the whole</title>
 <link rel="stylesheet" href="css/estilos.css" />
+<link rel="stylesheet" href="css/estilosJuego.css" />
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="fontawesome/css/all.css" />
 <link rel="icon" href="img/logo.png" type="image/x-icon" />
