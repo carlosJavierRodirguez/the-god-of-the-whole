381652e3792fb0e575bdd5ae8a073e18e095ce36
criss300 <cristianelpro004@gmail.com>
2024-10-10 05:06:32 -0500

    10/10/2024 5:06 a. m. se completo la mayoria del diseño queda pendiente arreglar bien el responsive y algunas imagenes

--- juego.php
+++ juego.php
@@ -6,45 +6,38 @@
   <link rel="stylesheet" href="css/estilosJuego.css"/>
 </head>
 
-<body>
-  <div class="container">
+<body class="pantano">
+  <div class="container-fluid">
 
     <!-- Encabezado de la ronda y tiempo -->
     <div class="muro">
-
       <div class="texto1">
-        <h2>Ronda:2</h2>
+        <h2>Ronda: 2</h2>
         <p>Tiempo: 5:00</p>
         <p>¿Qué dioses se consideran olímpicos?</p>
       </div>
     </div>
 
     <!-- Conjunto A: Personaje mostrado -->
-    <div class="text-center mb-4">
-      <div class="card">
-        <div class="card-body">
-          <h5>Conjunto A</h5>
-        </div>
-      </div>
+    <div class="pergaminoveneno">
+      <h5></h5>
+      <div class="espadas"></div>
     </div>
 
     <!-- Botones de estado -->
-    <div class="text-center mb-4">
-      <button class="btn btn-success mx-2">¿Terminaste?</button>
-      <button class="btn btn-danger mx-2">Listo</button>
-    </div>
+
 
     <!-- Selección de personajes/dioses -->
     <div class="row my-4 justify-content-center">
-      <div class="col-md-4 text-center mb-4">
+      <div class="col-6 col-md-4 text-center mb-4">
         <img src="ruta-imagen-afrodita" alt="Afrodita" class="img-fluid">
         <p>Afrodita</p>
       </div>
-      <div class="col-md-4 text-center mb-4">
+      <div class="col-6 col-md-4 text-center mb-4">
         <img src="ruta-imagen-hera" alt="Hera" class="img-fluid">
         <p>Hera</p>
       </div>
-      <div class="col-md-4 text-center mb-4">
+      <div class="col-6 col-md-4 text-center mb-4">
         <img src="ruta-imagen-atenea" alt="Atenea" class="img-fluid">
         <p>Atenea</p>
       </div>
