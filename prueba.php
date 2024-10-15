dc999701c46b068f1bcb8a03d72c159f0aaa3dc8
criss300 <cristianelpro004@gmail.com>
2024-10-14 23:24:22 -0500

    14/10/2024 me duelen los ojos que rabia

--- prueba.php
+++ prueba.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<?php include('principal/head.php'); ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/estilosJuego.css" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <?php include('principal/head.php'); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilosJuego.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 </head>
 <body class="pantano">
     <div class="grid-container">
-        <div class="item1 muro">1</div>
-        <div class="item2">2</div>
-        <div class="item3">3</div>
+        <div class="item1">
+            <div class="muro"></div>
+        </div>
+        <div class="item2">
+        <div class="espadas">
+                   <!-- Contenido de las espadas -->
+                </div>
+            <div class="inner-item5">
+            </div>
+        </div>
+        <div class="item3">
+            <div class="arboles"></div> <!-- Contenedor apropiado para los arboles -->
+        </div>
         <div class="item4">
-            <div class="inner-item4">4</div>
-            <div class="inner-item5">5</div>
+            <div class="inner-item5">
+            </div>
+            <div class="inner-item4">
+                <div class="pergaminoveneno">
+                    <!-- Contenido del pergaminoveneno -->
+                </div>
+            </div>
         </div>
     </div>
 </body>
