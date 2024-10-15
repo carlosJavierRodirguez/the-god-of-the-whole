46cc141a1301458fb2a32edb0ae1504c9a9780c1
criss300 <cristianelpro004@gmail.com>
2024-10-14 19:36:26 -0500

    14/10/2024 avances del cambio de grid

---
+++ prueba.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<?php include('principal/head.php'); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilosJuego.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+</head>
+<body class="pantano">
+    <div class="grid-container">
+        <div class="item1 muro">1</div>
+        <div class="item2">2</div>
+        <div class="item3">3</div>
+        <div class="item4">
+            <div class="inner-item4">4</div>
+            <div class="inner-item5">5</div>
+        </div>
+    </div>
+</body>
+</html>
