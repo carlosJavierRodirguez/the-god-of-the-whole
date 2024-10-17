fb4a7269050fcdf851f679c00fa14a26fa1043e8
criss300 <crisstrabajos004gmail.com>
2024-10-17 11:20:54 -0500

    17/10/2024 inicialziacion del arreglo de los movimientos del juego juego/juego.php

--- juego/juego.php
+++ juego/juego.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <?php include('principal/head.php'); ?>
+    <?php include('../principal/head.php'); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilosJuego.css" />
+    <link rel="stylesheet" href="../css/estilosJuego.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 </head>
 <body class="pantano">
@@ -15,11 +15,18 @@
             <div class="arboles">
             <div id="drag-container">
 
-        <div class="draggable afrodita" draggable="true" id="item1"></div>
+        <div class="draggable" draggable="true" id="item1"></div>
 
         <div class="draggable" draggable="true" id="item2"></div>
 
-        <div class="draggable " draggable="true" id="item3"><div class=""></div></div>
+        <div class="draggable " draggable="true" id="item3"></div>
+
+        <div class="draggable" draggable="true" id="item4"></div>
+
+        <div class="draggable" draggable="true" id="item5"></div>
+
+        <div class="draggable" draggable="true" id="item6"></div>
+        
 
     </div>
             </div>
@@ -36,6 +43,6 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="js/dragDropScript.js"></script>
+    <script src="../js/dragdrop.js"></script>
 </body>
 </html>
