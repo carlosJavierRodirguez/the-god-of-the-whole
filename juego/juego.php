8be5094cf7cc2fd58d1c348ee054debcd1f329d0
criss300 <cristianelpro004@gmail.com>
2024-12-05 00:54:48 -0500

    4/12/2024 se añadieron los 2 gif importantes

--- juego/juego.php
+++ juego/juego.php
@@ -67,7 +67,7 @@ include('../libreria/juego/listarPreguntas.php');
                     <i class="fa-solid fa-music"></i>
                 </button>
                 <div class="border rounded p-2 mt-2">
-                    <h1 class="text-white text-center">gif aca</h1>
+                    <div class="zeus-gif">..</div>
                 </div>
             </div>
 
@@ -120,7 +120,7 @@ include('../libreria/juego/listarPreguntas.php');
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-center">
-                    <img src="../gif/Mano.gif" alt="GIF" class="img-fluid rounded">
+                    <img src="../gif/pregunta.gif" alt="GIF" class="img-fluid rounded">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
